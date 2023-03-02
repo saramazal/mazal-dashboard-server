5fb2be0e62e0719bc19789b969e29da9b8e4fc70
Sara Mazal <mazalwebdesign@gmail.com>
2023-03-02 11:08:19 +0200

    user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,31 @@
 import User from '../mongodb/models/user.js';
 
-const getAllUsers = async (req, res) => {};
-const createUser = async (req, res) => {};
-const getUserInfoById = async (req, res) => {};
+const getAllUsers = async (req, res) => { };
+
+const createUser = async (req, res) => {
+    try {
+        const { name, email, avatar } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(200).json({ userExists });
+
+        const newUser = await User.create({
+            name,
+            email,
+            avatar
+        })
+
+        res.status(200).json(newUser);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
+const getUserInfoById = async (req, res) => { };
+
 
 export {
     getAllUsers,
