d81b2dbb040ee4bd1bf4174a90b70a04835d8a79
Sara Mazal <mazalwebdesign@gmail.com>
2023-03-02 13:18:18 +0200

    adding mongoose to property controller

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -1,6 +1,7 @@
 import Property from '../mongodb/models/property.js';
 import User from '../mongodb/models/user.js';
 
+import mongoose from "mongoose";
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -18,39 +19,43 @@ const getPropertyDetails = async (req, res) => {};
 
 const createProperty = async (req, res) => {
     try {
-       const { title, description, propertyType, location, price, photo, email } = req.body; 
+        const {
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo,
+            email,
+        } = req.body;
 
-       // Start a new session ...
-       const session = await mongoose.startSession();
-       session.startTransaction(); 
-       
-       const user = await User.findOne({ email}).session(session);
+        const session = await mongoose.startSession();
+        session.startTransaction();
 
-        if(user) throw new Error('User not found');
+        const user = await User.findOne({ email }).session(session);
+
+        if (!user) throw new Error("User not found");
 
         const photoUrl = await cloudinary.uploader.upload(photo);
 
-        const newProperty = await Property.create({ 
+        const newProperty = await Property.create({
             title,
             description,
             propertyType,
             location,
             price,
             photo: photoUrl.url,
-            creator: user._id
-        })
-        user.getAllProperties.push(newProperty._id);
+            creator: user._id,
+        });
+
+        user.allProperties.push(newProperty._id);
         await user.save({ session });
 
         await session.commitTransaction();
 
-        res.status(200).json({ message: 'Property created seccessfully!'})
-
+        res.status(200).json({ message: "Property created successfully" });
     } catch (error) {
-        res.status(500).json({ message: 'Error creating property!'})
-
-       
-        
+        res.status(500).json({ message: error.message });
     }
 };
 
