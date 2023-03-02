d9f51928980ee1676bd6f65e2ecf845b203c61f8
Sara Mazal <mazalwebdesign@gmail.com>
2023-03-02 09:16:04 +0200

    models user

--- mongodb/connect.js
+++ mongodb/connect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-const connectDB = (url) = {
-    mongoose.set('stricyQuery', true);
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true);
     mongoose.connect(url)
     .then(() => console.log('MongoDB connected!'))
     .catch((error) => console.log(error));
