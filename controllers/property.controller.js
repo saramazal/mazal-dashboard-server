41cf4e7534084f3fe23006f27c88e2861ea4f0f3
Sara Mazal <mazalwebdesign@gmail.com>
2023-03-02 10:11:02 +0200

    adding routes and controllers in index.js

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -3,4 +3,15 @@ import User from '../mongodb/models/user';
 
 const getAllProperties = async (req, res) => {};
 const getPropertyDetails = async (req, res) => {};
+const createProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {};
+
+export {
+    getAllProperties,
+    getPropertyDetails,
+    createProperty,
+    updateProperty,
+    deleteProperty
+}
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,16 @@
 import express from "express";
 
-
 // Import All Controllers:
+import {
+    getAllUsers,
+    createUser,
+    getUserInfoById
+} from '../controllers/user.controller.js';
 
+const router = express.Router();
+
+router.route('/').get(getAllUsers);
+router.route('/').post(createUser);
+router.route('/:id').get(getUserInfoById);
+
+export default router;
