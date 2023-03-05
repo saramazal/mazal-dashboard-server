e7530bdcb460540154cd5a352af0ac61b638f586
Sara Mazal <mazalwebdesign@gmail.com>
2023-03-05 10:24:17 +0200

    property controller

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -13,7 +13,38 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const getAllProperties = async (req, res) => {};
+const getAllProperties = async (req, res) => {
+    const {_end, _order, _start, _sort, title_like = "", propertyType= "" } = req.query;
+
+    const query = {};
+
+    if(propertyType !=='') {
+        query.propertyType = propertyType;
+    }
+
+    if(title_like) [
+        query.title = { $regex: title_like, $options: 'i' }
+    ]
+
+
+    try {
+        const count = await Property.countDocuments({query});
+
+        const properties = await Property
+          .find(query)
+          .limit(_end)
+          .skip(_start)
+          .sort({ [_sort]: _order})
+
+          res.header('x-total-count', count);
+          res.header('Access-Control-Expose-Headers', 'x=tptal-count');
+          res.status(200).json(properties);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
 const getPropertyDetails = async (req, res) => {};
 
 
